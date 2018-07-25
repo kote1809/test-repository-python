f3f1709d95b620e3f95d354b87c7dd5c179e13e9
kote1809 <41379676+kote1809@users.noreply.github.com>
2018-07-25 14:46:53 +0300

    refactor add contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,70 +1,58 @@
-# -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 import pytest
 from selenium import webdriver
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
     wd.implicitly_wait(60)
     return wd
-    
-def test_test_add_contact(driver):
-    success = True
-    driver.get("http://localhost/addressbook/group.php")
-    driver.find_element_by_name("user").click()
-    driver.find_element_by_name("user").clear()
-    driver.find_element_by_name("user").send_keys("admin")
-    driver.find_element_by_name("pass").click()
-    driver.find_element_by_name("pass").clear()
-    driver.find_element_by_name("pass").send_keys("secret")
-    driver.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+
+def logout(driver):
+    driver.find_element_by_link_text("Logout").click()
+
+
+def create_new_contact(driver, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work,
+                       fax, email, byear):
     driver.find_element_by_link_text("add new").click()
     driver.find_element_by_name("firstname").click()
     driver.find_element_by_name("firstname").clear()
-    driver.find_element_by_name("firstname").send_keys("FN")
+    driver.find_element_by_name("firstname").send_keys(firstname)
     driver.find_element_by_name("middlename").click()
     driver.find_element_by_name("middlename").clear()
-    driver.find_element_by_name("middlename").send_keys("MN")
+    driver.find_element_by_name("middlename").send_keys(middlename)
     driver.find_element_by_name("lastname").click()
     driver.find_element_by_name("lastname").clear()
-    driver.find_element_by_name("lastname").send_keys("LN")
+    driver.find_element_by_name("lastname").send_keys(lastname)
     driver.find_element_by_name("nickname").click()
     driver.find_element_by_name("nickname").clear()
-    driver.find_element_by_name("nickname").send_keys("NN")
+    driver.find_element_by_name("nickname").send_keys(nickname)
     driver.find_element_by_name("title").click()
     driver.find_element_by_name("title").clear()
-    driver.find_element_by_name("title").send_keys("Title")
+    driver.find_element_by_name("title").send_keys(title)
     driver.find_element_by_name("company").click()
     driver.find_element_by_name("company").clear()
-    driver.find_element_by_name("company").send_keys("Company")
+    driver.find_element_by_name("company").send_keys(company)
     driver.find_element_by_name("address").click()
     driver.find_element_by_name("address").clear()
-    driver.find_element_by_name("address").send_keys("Street")
+    driver.find_element_by_name("address").send_keys(address)
     driver.find_element_by_name("home").click()
     driver.find_element_by_name("home").clear()
-    driver.find_element_by_name("home").send_keys("123")
+    driver.find_element_by_name("home").send_keys(home)
     driver.find_element_by_name("mobile").click()
     driver.find_element_by_name("mobile").clear()
-    driver.find_element_by_name("mobile").send_keys("123")
+    driver.find_element_by_name("mobile").send_keys(mobile)
     driver.find_element_by_name("work").click()
     driver.find_element_by_name("work").clear()
-    driver.find_element_by_name("work").send_keys("123")
+    driver.find_element_by_name("work").send_keys(work)
     driver.find_element_by_name("fax").click()
     driver.find_element_by_name("fax").clear()
-    driver.find_element_by_name("fax").send_keys("123")
+    driver.find_element_by_name("fax").send_keys(fax)
     driver.find_element_by_name("email").click()
     driver.find_element_by_name("email").clear()
-    driver.find_element_by_name("email").send_keys("r@d.ru")
+    driver.find_element_by_name("email").send_keys(email)
     driver.find_element_by_name("homepage").click()
     if not driver.find_element_by_xpath("//div[@id='content']/form/select[1]//option[21]").is_selected():
         driver.find_element_by_xpath("//div[@id='content']/form/select[1]//option[21]").click()
@@ -72,13 +60,38 @@ def test_test_add_contact(driver):
         driver.find_element_by_xpath("//div[@id='content']/form/select[2]//option[13]").click()
     driver.find_element_by_name("byear").click()
     driver.find_element_by_name("byear").clear()
-    driver.find_element_by_name("byear").send_keys("1990")
+    driver.find_element_by_name("byear").send_keys(byear)
     driver.find_element_by_name("theform").click()
     driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-    driver.find_element_by_link_text("Logout").click()
-    
+
+
+def login(driver, username, password):
+    driver.find_element_by_name("user").click()
+    driver.find_element_by_name("user").clear()
+    driver.find_element_by_name("user").send_keys(username)
+    driver.find_element_by_name("pass").click()
+    driver.find_element_by_name("pass").clear()
+    driver.find_element_by_name("pass").send_keys(password)
+    driver.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+
+def go_to_home_page(driver):
+    driver.get("http://localhost/addressbook/group.php")
+
+
+def test_add_empty_contact(driver):
+    go_to_home_page(driver)
+    login(driver, username="admin", password="secret")
+    create_new_contact(driver, firstname="FN", middlename="MN", lastname="LN", nickname="NN", title="Title", company="Company", address="Street", home="123", mobile="123", work="123", fax="123",
+                       email="r@d.ru", byear="1990")
+    go_to_home_page(driver)
+    create_new_contact(driver, firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
+                       home="", mobile="", work="", fax="",
+                       email="", byear="")
+    logout(driver)
+
 def tearDown(driver):
-    self.wd.quit()
+    driver.wd.quit()
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test_add_new_group.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_new_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(30)
+    
+    def test_test_add_new_group(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("ww")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("ee")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("rr")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("Logout").click()
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
